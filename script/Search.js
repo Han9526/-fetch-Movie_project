192c5cd09ca74c04b03e10c29451bd559e230e4b
한승준 <jk2tmdwns@naver.com>
2023-10-23 16:29:02 +0900

    fin

--- script/Search.js
+++ script/Search.js
@@ -2,7 +2,7 @@
  function Searchfunction() {
     const a = document.getElementsByClassName("movie-card");
     for (let i = 0; i < a.length; i++) {
-        let movieTitle = document.getElementById(`${a[i].dataset.id}`).innerHTML;
+        let movieTitle = document.getElementById(`title-${a[i].dataset.id}`).innerHTML;
         let inputField = document.getElementById("input_field").value;
         if (movieTitle.toLowerCase().indexOf(inputField.toLowerCase()) !== -1) {
             a[i].style.display = "block"
